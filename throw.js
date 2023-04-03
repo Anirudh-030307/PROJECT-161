144d7b93d0c15736b81b3e7051b204760d92ed21
Anirudh-030307 <65300694+Anirudh-030307@users.noreply.github.com>
2023-04-03 18:18:43 +0530

    Update throw.js

--- throw.js
+++ throw.js
@@ -20,18 +20,11 @@ AFRAME.registerComponent("bowling-balls", {
           y: pos.y-1.2,
           z: pos.z,
         });
-
         var camera = document.querySelector("#camera").object3D;
-
-        //get the camera direction as Three.js Vector
         var direction = new THREE.Vector3();
         camera.getWorldDirection(direction);
-
-        //set the velocity and it's direction
         ball.setAttribute("velocity", direction.multiplyScalar(-10));
-
         var scene = document.querySelector("#scene");
-
         scene.appendChild(ball);
       }
     });
